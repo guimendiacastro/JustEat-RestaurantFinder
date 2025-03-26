4fedeebcf52a99118efa9331b3a98076fbd8a992
Gui Castro <guibobonecastro@gmail.com>
2025-03-26 07:37:06 +0000

    Initial Commit

---
+++ RestaurantFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestaurantFinder
+//
+//  Created by Gui Castro on 26/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestaurantFinder/RestaurantFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantFinderApp.swift
+//  RestaurantFinder
+//
+//  Created by Gui Castro on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
